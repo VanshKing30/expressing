1e706bb22c2926e86ccd715f3f3f3e7d62efb1be
Vansh <vanshwaldeo360@gmail.com>
2024-04-09 16:33:54 +0530

    Added more route

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.get('/' , (req , res) =>{
     res.send("Hello Vansh");
 })
 
+app.get('/api/v1' , (req , res)=>{
+    res.send("You are at the api page");
+})
+
 app.listen(PORT , ()=>{
     console.log(`The app is listninig on ${PORT}`);
 });
