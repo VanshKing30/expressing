157e5a6b4dd81aeab0a82847fec5c56ef8c434e2
Vansh <vanshwaldeo360@gmail.com>
2024-04-09 17:53:15 +0530

    Adeed more post reoutes

--- index.js
+++ index.js
@@ -11,6 +11,16 @@ app.get('/api/v1' , (req , res)=>{
     res.send("You are at the api page");
 })
 
+app.post('/home' , (req , res)=>{
+    const {title, des} = req.body();
+    if(!title){
+        console.log("Title was not provoided");
+    }
+    else{
+        res.send("Hey homie");
+    }
+})
+
 app.post('./api/v1/posts' , (req , res) =>{
     res.send("Post has been recieved")}
 )
