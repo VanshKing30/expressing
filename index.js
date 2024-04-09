a4d8d84d0265dbdf8e72e3639d37febb80d5234f
Vansh <vanshwaldeo360@gmail.com>
2024-04-09 17:45:21 +0530

    added first post req

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ app.get('/api/v1' , (req , res)=>{
     res.send("You are at the api page");
 })
 
+app.post('./api/v1/posts' , (req , res) =>{
+    res.send("Post has been recieved")}
+)
+
 app.listen(PORT , ()=>{
     console.log(`The app is listninig on ${PORT}`);
 });
